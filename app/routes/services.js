f38a647778bc565a87c90316e79693e0223aeef1
Arturo Luna <arturoluna879@gmail.com>
2020-12-02 15:40:16 -0400

    final

--- app/routes/services.js
+++ app/routes/services.js
@@ -20,8 +20,6 @@ const trimRequest = require('trim-request')
  */
 router.get(
   '/',
-  requireAuth,
-  AuthController.roleAuthorization(['user', 'admin', 'office', 'tecnico']),
   trimRequest.all,
   controller.getItems,
   db.auditoriaMethods
